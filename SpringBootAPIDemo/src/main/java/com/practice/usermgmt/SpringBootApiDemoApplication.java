a26f4a84248b2cb5093de5667c67c3c50d93a71d
Gautam Trivedi <gautam.trivedi@gmail.com>
2017-03-05 11:05:18 -0600

    Initial Commit

---
+++ SpringBootAPIDemo/src/main/java/com/practice/usermgmt/SpringBootApiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.practice.usermgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiDemoApplication.class, args);
+	}
+}
